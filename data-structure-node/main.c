121b172cc26abafc89981f3079de29b81daef94a
Purin.P <dev.purin@gmail.com>
2025-10-09 22:16:15 +0700

    feat: data-structure-node

---
+++ data-structure-node/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+typedef struct dataNode {
+    int number;
+    struct  dataNode *next;
+} data;
+
+data *headA = NULL, *prevA = NULL;
+data *headB = NULL, *prevB = NULL;
+data *headC = NULL, *prevC = NULL;
+data *deposit = NULL, *prevDeposit = NULL;
+
+int main() {
+    
+}
